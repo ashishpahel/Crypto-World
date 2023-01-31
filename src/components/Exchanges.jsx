c185b4b8b0d245a2789eec8a2751f04e05240fea
ashishpahel <ashishpahel1999@gmail.com>
2023-01-31 16:54:26 +0530

    first commit

---
+++ src/components/Exchanges.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { server } from "../index";
+import {
+  Container,
+  HStack,
+} from "@chakra-ui/react";
+import Loader from "./Loader";
+import ErrorComponent from "./ErrorComponent";
+import ExchangeCard from "./ExchangeCard";
+
+const Exchanges = () => {
+  const [exchanges, setExchanges] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchExchanges = async () => {
+      try {
+        const { data } = await axios.get(`${server}/exchanges`);
+        setExchanges(data);
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchExchanges();
+  }, []);
+
+  if (error)
+    return <ErrorComponent message={"Error While Fetching Exchanges"} />;
+
+  return (
+    <Container maxW={"container.xl"}>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <HStack wrap={"wrap"} justifyContent={"space-evenly"}>
+            {exchanges.map((i) => (
+              <ExchangeCard
+                key={i.id}
+                name={i.name}
+                img={i.image}
+                rank={i.trust_score_rank}
+                url={i.url}
+              />
+            ))}
+          </HStack>
+        </>
+      )}
+    </Container>
+  );
+};
+
+
+
+export default Exchanges;
